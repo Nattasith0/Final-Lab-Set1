ed5437260b4b30a4a3317907dc2b41c30479fd03
Nattasith0 <gunkung.x20@gmail.com>
2026-03-15 11:08:04 +0700

    feat(scripts): add gen-certs.sh and gen-hash.js

--- scripts/gen-hash.js
+++ scripts/gen-hash.js
@@ -1,6 +1,8 @@
 const bcrypt = require('bcrypt');
+
 async function main() {
-    const hash = await bcrypt.hash('password123', 10);
-    console.log(hash);
+  console.log('alice123:', await bcrypt.hash('alice123', 10));
+  console.log('bob456:', await bcrypt.hash('bob456', 10));
+  console.log('adminpass:', await bcrypt.hash('adminpass', 10));
 }
 main();
